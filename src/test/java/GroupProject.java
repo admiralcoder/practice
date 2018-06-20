064d27fa2d47c2c8803ad2a2f88ce981b62ddc8c
Cihan Aygunes <cihanaygunes@hotmail.com>
2018-06-19 23:23:49 -0400

    Test Case #6 - version 1.0

--- src/test/java/GroupProject.java
+++ src/test/java/GroupProject.java
@@ -1,3 +1,4 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -10,6 +11,26 @@ public class GroupProject {
 		WebDriverManager.chromedriver().setup();
 		WebDriver driver = new ChromeDriver();
 		
+		driver.manage().window().fullscreen();
+		
+		driver.get("http://www.weatherbase.com/");
+		driver.findElement(By.linkText("North America")).click();
+		driver.findElement(By.linkText("United States of America")).click();
+		driver.findElement(By.linkText("District of Columbia")).click();
+		
+		
+		String text1 = driver.findElement(By.xpath("(//td[@class='datacunit'])[1]")).getText();
+		String text2 = driver.findElement(By.xpath("(//td[@class='datacunit'])[2]")).getText();
+		String text3 = driver.findElement(By.xpath("(//td[@class='datacunit'])[3]")).getText();
+		
+		if(text1.equals("Average Temperature (F)") && text2.equals("Average High Temperature (F)") && text3.equals("Average Low Temperature (F)")) {
+			System.out.println("pass");
+		}else {
+			System.out.println("fail");
+			System.out.println(text1 + " is not a Fahrenheit type");
+		}
+		
+		driver.close();
 
 	}
 
